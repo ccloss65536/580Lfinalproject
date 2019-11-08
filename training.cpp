530d127c4f3cd2e793421663c3631cafd445ddac
kevinkyan <kevyan1998@gmail.com>
2019-11-08 12:49:44 -0500

    neural network framework comments

---
+++ training.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+//take training image file name
+
+//take training labels
+
+//num training samples
+
+//Image size dimensions(width,heigh)
+
+//num input neurons
+//num hidden neurons
+//num output neurons
+//epochs?
+//learning rate
+//momentum
+//epsilon
+
+//input layer to hidden layer
+
+//input hidden later to output layer
+
+//Output layer
+
+//Image
+
+//filestream to read data
+
+//allocate memorry
+
+//ELU Function
+
+//perceptron
+
+//Back Propogation Algo
+
+//Learning
+
+//Reading input
+
+//save weights
+
+//main
