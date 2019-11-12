11d960d961efe7ca92e878a01e6bcaa6014ee4bb
Science V <ccloss1@binghamton.edu>
2019-11-12 00:22:28 -0500

    claim, 1 local variable

--- training.cpp
+++ training.cpp
@@ -6,6 +6,7 @@
 #include <Eigen/Dense> 
 
 using namespace std;
+using namespace Eigen;
 class NeuralNetwork{
 //take training image file name
 
@@ -15,13 +16,20 @@ class NeuralNetwork{
 
 
 
-//num input neurons
+int num_input_neurons;
 //num hidden neurons
+int num_hidden_neurons;
 //num output neurons
+int num_output_neurons;
 //epochs?
 //learning rate
+double learning_rate;
+vector<MatrixXf> layers;
+
+//epochs?
+//learning rate Carl
 //momentum
-//epsilon
+//epsilon Carl
 
 //input layer to hidden layer Carl
 
@@ -35,8 +43,20 @@ class NeuralNetwork{
 
 //allocate memory Carl
 
-//ELU Function
-//d/dx ELU 
+//ELU Function Kevin Yan
+double ELU(double x) {
+	if(x > 0) 
+		return x;
+	else 
+		return learningRate*(exp(x)-1);
+}
+//ELU derivative Function Kevin Yan
+double dELU(double x) {
+	if(x > 0)
+		return 1;
+	else
+		return ELU(x) + learningRate;
+ 
 
 //perceptron
 
