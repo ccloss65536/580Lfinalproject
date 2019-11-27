4d919cfcf42fcfd155f513da213d742e2c6ca7df
Science V <ccloss1@binghamton.edu>
2019-11-26 22:35:09 -0500

    claim, constructor

--- training.cpp
+++ training.cpp
@@ -5,12 +5,13 @@
 #include <thread>
 #include <mutex>
 #include <condition_variable>
-#include <random>
 #include <Eigen/Dense> 
 #include <cmath>
 
 using namespace std;
 using namespace Eigen;
+
+
 
 
 class NeuralNetwork{
@@ -28,41 +29,43 @@ public:
 	int num_hidden_neurons;
 	//num output neurons
 	int num_output_neurons;
-	//epochs?
+	int epochs;
 	//learning rate
 	double learning_rate;
 	vector<MatrixXd> layers;
-	mutex loss_sum_lock, producer, consumer, file; //Do these need to be pointers to mutexes? probably not
+	mutex loss_sum_lock, producer, consumer, file;
 	condition_variable full, empty;
 	vector<pair<VectorXd>> vecs_to_calc; //put input and expected output vectors into this, and take them out to process
 	unsigned int pro_buffer_index, con_buffer_index, buffer_size, con_training_total, pro_training_total, buffer_taken;
 	unsigned int num_producers, num_consumers;
 	
-	NeuralNetwork(double learning_rate, num_layers/*...*/){
+	NeuralNetwork(double learning_rate, int num_layers, int epochs, int hidden_layer_size, int buffer_size){
+		num_input_neurons = 784; //28*28, the number of pixels in a MNIST image
+		num_output_neurons = 10;
 		this->learning_rate = learning_rate;
+		this->buffer_size = buffer_size
 		pro_buffer_index = con_buffer_index = con_training_total = buffer_taken = 0;
-		//initalize vector buffer
+		vecs_to_calc = vector(buffer_size);
+		/* We use an extra hidden "neuron" to perpetuate the bias term, the
+		   first element of each set of inputs
+		*/
+		layers.emplace(layers.end(), MatrixXd::Random(num_input_neurons + 1, hidden_layer_size + 1));
+		for(int i = 0; i  < num_layers - 2; i++){
+			layers.emplace(layers.end(), MatrixXd::Random(hidden_layer_size + 1, hidden_layer_size + 1));
+		}
+		layers.emplace(layers.end(), MatrixXd::Random(hidden_layer_size + 1, num_output_neurons));
+		this->epochs = epochs;
+
+				
 
 		//TODO: finish this
 
 	}
-		
-
-	//epochs?
-	//epsilon Carl
-
-	//input layer to hidden layer Carl
-
-	//input hidden layer(s) to output layer Carl
-
-	//Output layer Carl
 
 	//Image
 
-	//filestream to read data
-
-	//allocate memory 
 	pair<VectorXd> generate_training_example(istream& file){
+		//TODO: finish this
 		//make sure to grab and ungrab the file mutex as necessary to avoid corruption 
 	}
 	
@@ -84,10 +87,6 @@ public:
 		while(training_total < training_size){
 			auto l = unique_lock(consumer); //lock is taken as soon as this object is constructed
 			while(buffer_taken <= 0){
-				if(file_ended){
-					l.unlock();
-					return;
-				}
 				empty.wait(l);
 			}
 
@@ -125,6 +124,7 @@ public:
 	//perceptron 
 	
 	//This returns the loss for a single example, preconverted into a vector
+	//Always pass Eigen matrices & vectors by reference!
 	double loss(const VectorXd& input_vector, const VectorXd& reference_vec){
 		VectorXd temp = input_vector;
 		for(MatrixXd l : layers){
@@ -140,21 +140,26 @@ public:
 	//My plan is to calculate loss on every training example in parallel, to add to a variable, then backpropagate (I am unclaimimng this part).
 
 	void train(const& string filename, unsigned int training_size){
-		double loss = 0;
-		ifstream f(filename);
-		vector<thread> threads;
-		for(unsigned int i = 0; i < num_producers; i++){
-			threads.push_back(thread(producer_thread, this, f, training_size));
-		}
-		for(unsigned int i = 0; i < num_consumers; i++){
-			threads.push_back(thread(consumer_thread, this, loss, training_size));
-		}
-		for(thread& t : threads){
-			t.join();
-		}
+		for(int i = 0; i < epochs; i++){
+			double loss = 0;
+			ifstream f(filename);
+			vector<thread> threads;
+			for(unsigned int i = 0; i < num_producers; i++){
+				threads.push_back(thread(producer_thread, this, f, training_size));
+			}
+			for(unsigned int i = 0; i < num_consumers; i++){
+				threads.push_back(thread(consumer_thread, this, loss, training_size));
+			}
+			for(thread& t : threads){
+				t.join();
+			} 
 		
 		
-		//backpropagate
+			//backpropagate
+
+			f.close();
+		}
+		
 
 	}
 		
@@ -163,7 +168,7 @@ public:
 
 
 
-	//Reading input
+	//Reading input Carl
 
 	//save weights
 };
