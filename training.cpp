3830ee4922889e0885042f60cbf0c589ca8d55ea
Science V <ccloss1@binghamton.edu>
2019-11-27 00:13:45 -0500

    input and output started

--- training.cpp
+++ training.cpp
@@ -13,6 +13,32 @@ using namespace Eigen;
 
 
 
+const int IMAGE_ROWS = 28
+const int IMAGE_COLS = 28
+const int IMAGE_SIZE =  IMAGE_ROWS * IMAGE_COLS; //28*28, the number of pixels in a MNIST image
+
+
+//WHYYY are MNIST numbers in big endian
+int read_num(char* buff, size_t size){
+	int out = 5;
+	char* test_p = &test;
+	if(*test_p){ //system is little endian, make sure to reverse the number
+		for(size_t i = size - 1; i <= 0; i+--){
+			*test_p = buff + i;
+			*test_p++;
+		}
+	}
+	else{
+		for(size_t i = 0; i < size; i++){
+			*test_p = buff + i;
+			*test_p++;
+		}
+	}
+	return out;
+}
+
+
+
 
 class NeuralNetwork{
 public:
@@ -33,14 +59,14 @@ public:
 	//learning rate
 	double learning_rate;
 	vector<MatrixXd> layers;
-	mutex loss_sum_lock, producer, consumer, file;
+	mutex loss_sum_lock, producer, consumer, generate;
 	condition_variable full, empty;
 	vector<pair<VectorXd>> vecs_to_calc; //put input and expected output vectors into this, and take them out to process
 	unsigned int pro_buffer_index, con_buffer_index, buffer_size, con_training_total, pro_training_total, buffer_taken;
 	unsigned int num_producers, num_consumers;
 	
 	NeuralNetwork(double learning_rate, int num_layers, int epochs, int hidden_layer_size, int buffer_size){
-		num_input_neurons = 784; //28*28, the number of pixels in a MNIST image
+		num_input_neurons = IMAGE_SIZE;
 		num_output_neurons = 10;
 		this->learning_rate = learning_rate;
 		this->buffer_size = buffer_size
@@ -64,13 +90,18 @@ public:
 
 	//Image
 
-	pair<VectorXd> generate_training_example(istream& file){
+	pair<VectorXd> generate_training_example(istream& images, istream& labels){
 		//TODO: finish this
-		//make sure to grab and ungrab the file mutex as necessary to avoid corruption 
+		char buff[IMAGE_SIZE];
+		auto l = unique_lock(generate);
+		images.read(buff, IMAGE_SIZE);
+		l.unlock();
+		VectorXd target();
+
 	}
 	
-	void producer_thread(istream& file, unsigned int training_size){
-		while(file && pro_training_total < training_size){
+	void producer_thread(istream& images, istream& labels, unsigned int training_size){
+		while(images && pro_training_total < training_size){
 			pair<VectorXd> example = generate_training_example(file);
 			auto l = unique_lock(producer);
 			while(buffer_taken >= buffer_size) full.wait(l);
@@ -139,13 +170,32 @@ public:
 	//Learning
 	//My plan is to calculate loss on every training example in parallel, to add to a variable, then backpropagate (I am unclaimimng this part).
 
-	void train(const& string filename, unsigned int training_size){
+	void train(const& string image_file, const string& label_file){
 		for(int i = 0; i < epochs; i++){
 			double loss = 0;
-			ifstream f(filename);
+			ifstream images(image_file);
+			ifstream labels(label_file);
+			char magic_buff[4];
+			images.read(magic, 4);
+			int magic = read_int(magic_buff, 4);
+
+			if(magic != 2051){
+				cerr << "Bad image data! " << magic << endl;
+				exit(magic);
+			}
+			labels.read(magic_buff, 4);
+			magic = read_int(magic_buff, 4);
+			if(magic != 2049){
+				cerr << "Bad label data! " << magic << endl;
+				exit(magic);
+			}
+
+			//TODO: read out dimensions and training size
+
+			
 			vector<thread> threads;
 			for(unsigned int i = 0; i < num_producers; i++){
-				threads.push_back(thread(producer_thread, this, f, training_size));
+				threads.push_back(thread(producer_thread, this, i, training_size));
 			}
 			for(unsigned int i = 0; i < num_consumers; i++){
 				threads.push_back(thread(consumer_thread, this, loss, training_size));
